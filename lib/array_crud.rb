1f68029e61a47b639b886376acac17bde3de99b7
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-23 23:53:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+array = []
 end
 
 def create_an_array
-  
+  array = ["num1", "num2", "num3", "num4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+arrays! = array.shift
 end
 
 def remove_element_from_start_of_array(array)
